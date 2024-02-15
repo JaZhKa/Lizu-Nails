76af63cb7de8ed9a41d3449840b3d7b422de0cc8
Yakov Lemenkov <yakoff4@gmail.com>
2024-02-15 23:06:11 +0300

    add tailwind&prettier plugin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-
 function App() {
-
   return (
     <>
-
+      <h1 className="text-xl italic text-slate-500 underline">Hollow</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
