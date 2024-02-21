98a9e3907c2f559c96d4a3c1383110aae90cb95e
Yakov Lemenkov <yakoff4@gmail.com>
2024-02-21 22:55:45 +0300

    add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import Header from "./components/Header";
 import { useSelector } from "react-redux";
 import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Button from "./components/elements/Button";
 
 function App() {
   const isActive = useSelector((state) => state.isActive.value);
   return (
     <div>
       <Header />
-      <main className={"min-h-[calc(100dvh_-_212px)]" + (isActive ? " -mt-64" : " mt-0")}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iste
-        ipsam atque eligendi veniam magnam adipisci quibusdam fuga porro sed
-        cupiditate deserunt deleniti, assumenda voluptas accusamus. Vel eos
-        omnis cumque repellat nihil accusamus voluptas consequuntur similique
-        molestiae earum.
+      <main
+        className={
+          "min-h-[calc(100dvh_-_212px)]" + (isActive ? " -mt-64" : " mt-0")
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Button />} />
+        </Routes>
       </main>
       <Footer />
     </div>
