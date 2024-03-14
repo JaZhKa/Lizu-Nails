75509aa336c53c5719b97b2e27911f04b70250a0
Yakov Lemenkov <yakoff4@gmail.com>
2024-03-14 22:36:06 +0300

    fix study page & add footer map

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,20 @@
 const Footer = () => {
   return (
-    <footer className="bottom-0 bg-text-color mt-5 grid grid-cols-2 items-center p-5">
+    <footer className="bottom-0 mt-5 grid grid-cols-2 md:grid-cols-3 items-center bg-text-color p-5">
       <div className="grid grid-rows-2 gap-6">
-        <p className="text-secondary text-xs">
+        <p className="text-xs text-secondary">
           Адрес: г. Минск, пер. Добромысленский, д.3.
         </p>
-        <p className="text-secondary text-xs">Контакты: (017) 239-40-75</p>
+        <p className="text-xs text-secondary">Контакты: (017) 239-40-75</p>
+      </div>
+      <div className="hidden md:grid justify-items-center">
+        <iframe
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3A1f6696b79cfcbb35e108cb6e494559d0c92cd0e3ff562da4484588b12b16cf47&amp;source=constructor"
+          width="320"
+          height="240"
+          frameBorder="0"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
       <div>
         <ul className="grid justify-items-end">
--- frontend/src/pages/Study.jsx
+++ frontend/src/pages/Study.jsx
@@ -8,59 +8,82 @@ const Study = () => {
       <h2 className="mb-16 mt-8 text-center text-6xl text-text-color md:text-left md:text-7xl xl:text-8xl">
         Обучение
       </h2>
-      <div className="flex flex-col gap-24 md:w-[768px]">
-        <article className="flex flex-col gap-8 items-center">
+      <div className="flex flex-col gap-24 md:w-[768px] lg:w-[976px]">
+        <article className="flex flex-col items-center gap-8">
           <h3 className="mb-16 text-center text-3xl text-text-color md:text-4xl xl:text-7xl">
             Базовый
           </h3>
-          <img
-            className="h-full w-4/5 object-cover"
-            src={courseImg1}
-            alt="base course image"
-          />
-          <ul>
-            <li className="mb-2 text-text-color">
-              <b>Основы и техники: </b>Базовый курс охватывает все от
-              безопасного маникюра до самых востребованных дизайнов.
-            </li>
-            <li className="mb-2 text-text-color">
-              <b>Практический опыт: </b>Ученики получают возможность отработать
-              навыки на практике в реальных условиях салона.
-            </li>
-            <li className="mb-2 text-text-color">
-              <b>Старт карьеры: </b>Курс предлагает инструменты для быстрого
-              старта в профессии, включая подготовку портфолио и помощь в
-              трудоустройстве.
-            </li>
-          </ul>
+          <div className="flex flex-col items-center md:flex-row lg:gap-[2vw]">
+            <img
+              className="h-full w-4/5 object-cover md:w-1/2"
+              src={courseImg1}
+              alt="base course image"
+            />
+            <ul>
+              <li className="mb-2 p-7 text-text-color">
+                <b>Основы и техники</b>
+                <br />
+                <p className="mt-7 text-text-color">
+                  Базовый курс охватывает все от безопасного маникюра до самых
+                  востребованных дизайнов.
+                </p>
+              </li>
+              <li className="pmb-2 p-7 text-text-color">
+                <b>Практический опыт</b> <br />
+                <p className="mt-7 text-text-color">
+                  Ученики получают возможность отработать навыки на практике в
+                  реальных условиях салона.
+                </p>
+              </li>
+              <li className="mb-2 p-7 text-text-color">
+                <b>Старт карьеры</b>
+                <br />
+                <p className="mt-7 text-text-color">
+                  Курс предлагает инструменты для быстрого старта в профессии,
+                  включая подготовку портфолио и помощь в трудоустройстве.
+                </p>
+              </li>
+            </ul>
+          </div>
           <Button>Заказать</Button>
         </article>
-        <article className="flex flex-col gap-8 items-center">
+        <hr className="w-48 self-center bg-text-color" />
+        <article className="flex flex-col items-center gap-8">
           <h3 className="mb-16 text-center text-3xl text-text-color md:text-4xl xl:text-7xl">
             Продвинутый
           </h3>
-          <img
-            className="h-full w-4/5 object-cover"
-            src={courseImg2}
-            alt="advance course image"
-          />
-          <ul>
-            <li className="mb-2 text-text-color">
-              <b>Углубленные знания: </b>Продвинутый курс расширяет базовые
-              навыки, включая работу с инновационными материалами и
-              технологиями.
-            </li>
-            <li className="mb-2 text-text-color">
-              <b>Специализированные техники: </b>Курс охватывает продвинутые
-              стили маникюра, такие как гель и шеллак, и предлагает детальное
-              изучение нейл-арта.
-            </li>
-            <li className="mb-2 text-text-color">
-              <b>Профессиональное развитие: </b>Предоставляется персональный
-              план развития и поддержка для быстрого старта и продвижения в
-              карьере мастера маникюра.
-            </li>
-          </ul>
+          <div className="flex flex-col items-center md:flex-row lg:gap-[2vw]">
+            <img
+              className="h-full w-4/5 object-cover md:w-1/2"
+              src={courseImg2}
+              alt="advance course image"
+            />
+            <ul>
+              <li className="mb-2 p-7 text-text-color">
+                <b>Углубленные знания</b>
+                <br />
+                <p className="mt-7 text-text-color">
+                  Продвинутый курс расширяет базовые навыки, включая работу с
+                  инновационными материалами и технологиями.
+                </p>
+              </li>
+              <li className="mb-2 p-7 text-text-color">
+                <b>Специализированные техники</b>
+                <br />
+                <p className="mt-7 text-text-color">
+                  Курс охватывает продвинутые стили маникюра, такие как гель и
+                  шеллак, и предлагает детальное изучение нейл-арта.
+                </p>
+              </li>
+              <li className="mb-2 p-7 text-text-color">
+                <b>Профессиональное развитие</b> <br />
+                <p className="mt-7 text-text-color">
+                  Предоставляется персональный план развития и поддержка для
+                  быстрого старта и продвижения в карьере мастера маникюра.
+                </p>
+              </li>
+            </ul>
+          </div>
           <Button>Заказать</Button>
         </article>
       </div>
